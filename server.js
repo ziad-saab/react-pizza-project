8b6f6a0b22598aa939d32daaa5730e87221cb110
Ziad Saab <ziad.saab@gmail.com>
2016-05-17 09:42:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+
+/*
+this says: serve all the files in the src directory if they match the URL
+
+For example, if the client requests http://server/css/app.css then the file in src/css/app.css will be served
+But if the client requests http://server/step-2 then since there is no file by that name the middleware will call next()
+*/
 app.use(express.static(__dirname + '/src'));
+
+/*
+This says: for any path NOT served by the middleware above, send the file called index.html instead.
+For example, if the client requests http://server/step-2 the server will send the file index.html. Then on the browser, React Router will load the appropriate component
+*/
 app.get('/*', function(request, response) {
   response.sendFile(__dirname + '/src/index.html');
 });
