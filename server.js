9fb01dce033f615da9f098fc34780acb1b7f623f
Ziad Saab <ziad.saab@gmail.com>
2016-05-17 09:44:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ But if the client requests http://server/step-2 then since there is no file by t
 */
 app.use(express.static(__dirname + '/src'));
 
+/* insert any app.get or app.post you need here. only if you do the advanced part */
+
 /*
 This says: for any path NOT served by the middleware above, send the file called index.html instead.
 For example, if the client requests http://server/step-2 the server will send the file index.html. Then on the browser, React Router will load the appropriate component
